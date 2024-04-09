2dfb8071bbfd576d606605fae4d0852d3cc870a5
Amaldev <amaldev.psn@gmail.com>
2024-04-09 21:16:05 +0530

    get and post working - app is working at this stage

---
+++ app/src/main/java/com/example/demo/FullNameRequest.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+public class FullNameRequest {
+
+    private String firstName;
+    private String lastName;
+
+    // getters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ app/src/main/java/com/example/demo/NameController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NameController {
+
+    @PostMapping("/fullname")
+    public String getFullName(@RequestBody FullNameRequest fullNameRequest) {
+        String firstName = fullNameRequest.getFirstName();
+        String lastName = fullNameRequest.getLastName();
+        String fullName = firstName + " " + lastName;
+        return fullName;
+    }
+}
